9d5565b3e37f75d3e423120b3ef92ebb2d89dcf0
alesplll <alex.golovin.vrn@gmail.com>
2023-10-04 19:21:50 +0300

    sht

--- lesson.java
+++ lesson.java
@@ -1,3 +1,4 @@
+// Михаил Витальевич - преподаватель
 package Java.lesson1;
 
 import java.util.Scanner;
@@ -8,10 +9,16 @@ public class lesson {
         while (scanner.hasNextLine()) {
             int a = scanner.nextInt();
             System.out.println(a % 10);
+            for (int i = 0; i < 4; i++) {
+                System.out.println(i);
+                if (i % 2 == 0) {
+                    System.out.println("Test: " + i * i + " ");
+                }
+
+            }
 
         }
         scanner.close();
-
     }
 
 }
