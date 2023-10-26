9475441a239f7c834521bb296fc4de4ab219c39a
alesplll <alex.golovin.vrn@gmail.com>
2023-10-26 20:52:33 +0300

    ex

---
+++ exam/temp1.java
@@ -0,0 +1,24 @@
+package Java.lesson1.exam;
+
+import java.util.Scanner;
+
+public class temp1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] array = new int[n][n];
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                array[i][j] = scanner.nextInt();
+
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                    sum += array[i][j];
+                }
+            }
+        }
+        System.out.println(sum);
+        scanner.close();
+    }
+}
---
+++ exam/temp2.java
@@ -0,0 +1,17 @@
+package Java.lesson1.exam;
+
+import java.util.Scanner;
+
+public class temp2 {
+    public static void main(String[] args) {
+        long i = 0, a = 2;
+        while (i < 10) {
+            a *= a;
+            System.out.println(a);
+            i++;
+        }
+    }
+
+}
+
+/// 9,223,372,036,854,775,807
